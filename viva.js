3f61d436671be51940396e83bcb1a845b142ba78
masineedi <masineediaidtya526@gmail.com>
2018-01-03 18:23:07 +0530

    initial commit

---
+++ viva.js
@@ -0,0 +1,25 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    autoHeight:false
+});
+
+
+
+
+$
+
+  
+$('.animate').scrolla();
+
+
+
+$('.animate').scrolla('bind');
+
+
+
